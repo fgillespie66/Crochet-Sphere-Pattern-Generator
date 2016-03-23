cdc7595c43581572f264c4c0e37c1f95ac9c2ea7
Mark Gillespie <mark.gillespie81@verizon.net>
2016-03-22 22:47:25 -0400

    fixed some formatting and rounding issues

--- CrochetPatternGenerator.js
+++ CrochetPatternGenerator.js
@@ -2,14 +2,15 @@ function CrochetPatternGenerator(gauge, radius, stitchHeight) {
 	var maximumRowIndex = Math.floor(Math.PI*radius/stitchHeight)
 	var previousCircumferenceOfNextRow = 0
 	document.getElementById("patternText").innerHTML = ""
+
 	for (var i = 0; i < maximumRowIndex; i ++) {
-		var circumferenceOfNextRow = 2*Math.PI*radius*Math.sin((i+1)*stitchHeight/radius)*gauge
-		document.getElementById("patternText").innerHTML += "Row " + i + " (" + Math.floor(previousCircumferenceOfNextRow) + " stitches total): increase by " + Math.floor(circumferenceOfNextRow - previousCircumferenceOfNextRow) + " stitches <br>" 
-		previousCircumferenceOfNextRow = circumferenceOfNextRow
+		var circumferenceOfNextRow = Math.floor(2*Math.PI*radius*Math.sin((i+1)*stitchHeight/radius)*gauge)
+		var increaseAmount = circumferenceOfNextRow - previousCircumferenceOfNextRow
+		if (increaseAmount > 0) {
+			document.getElementById("patternText").innerHTML += "Row " + i + " (" + previousCircumferenceOfNextRow + " stitches total): increase by " + increaseAmount + " stitches <br>" 
+		} else {
+			document.getElementById("patternText").innerHTML += "Row " + i + " (" + previousCircumferenceOfNextRow + " stitches total): decrease by " + Math.abs(increaseAmount) + " stitches <br>" 
+		}previousCircumferenceOfNextRow = circumferenceOfNextRow
 
 	}
-
-	console.log(gauge)
-	console.log(radius)
-	console.log(stitchHeight) 
 }
